f5afb5b755b8173695ca8ee2e221ca1b077067e9
Sallehhuddin95 <62774625+Sallehhuddin95@users.noreply.github.com>
2023-08-27 22:26:16 +0800

    Implement validation for net asset value field
    feat:
    - non-negative value validation for net asset value field

--- backend/myapp/serializers.py
+++ backend/myapp/serializers.py
@@ -12,5 +12,11 @@ class InvestmentFundSerializer(serializers.ModelSerializer):
         if value < -100:
             raise serializers.ValidationError("Performance cannot be less than -100%")
         return value
+    
+    # validate net_asset_value field to be positive value
+    def validate_net_asset_value(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Net Asset Value cannot be negative")
+        return value
 
 
